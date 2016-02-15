a68a90721cab22c3092a0373772f5bd6fa33da2c
Liz Abinante <eabinante@gmail.com>
2016-02-15 11:51:50 -0800

    send api calls through proxy requests with signed info from sessions

--- app.rb
+++ app.rb
@@ -2,12 +2,22 @@ require 'sinatra'
 require 'sinatra/json'
 require 'omniauth-ravelry'
 require 'json'
+require 'net/http'
+require 'oauth'
 require './helpers'
 
 # dev gems
 require 'pry' if development?
 require 'pry-nav' if development?
 require 'sinatra/reloader' if development?
+
+configure :development do
+  URL_BASE = 'http'
+end
+
+configure :production do
+  URL_BASE = 'https'
+end
 
 configure do
   set :sessions, true
@@ -18,12 +28,12 @@ configure do
 end
 
 get '/' do
-  redirect to('/auth/ravelry') unless current_user
+  redirect to('/auth/ravelry') unless current_user?
   set_user unless @user
   json @user
 end
 
-post '/logout' do
+get '/logout' do
   clear_session
 end
 
@@ -35,4 +45,18 @@ get '/auth/ravelry/callback' do
   set_session
   set_user
   json @user
+end
+
+get '/api/*' do
+  url = params['splat'][0]
+  uri = URI("#{URL_BASE}://api.ravelry.com/#{url}")
+  req = Net::HTTP::Get.new(uri)
+
+  consumer = OAuth::Consumer.new(session[:consumer][:key], session[:consumer][:secret], { site: "#{URL_BASE}://api.ravelry.com"})
+  access_token = OAuth::AccessToken.new(consumer, session[:token][:token], session[:token][:secret]) 
+  consumer.sign!(req, access_token)
+
+  res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+
+  json JSON.parse(res.body)
 end
--- helpers.rb
+++ helpers.rb
@@ -1,28 +1,39 @@
 helpers do
-  def current_user
+  def current_user?
     !session[:uid].nil?
   end
 
   def set_user
     info = session[:info]
-    @user = { username: info['nickname'], first_name: info['first_name'], logged_in: current_user }
+    @user = { username: info['nickname'], first_name: info['first_name'], logged_in: current_user? }
   end
 
   def clear_session
     session[:info] = nil
     session[:uid] = nil
+    session[:consumer] = nil
+    session[:token] = nil
     @user = nil
   end
 
   def set_session
+    oauth_data = request.env['omniauth.auth']['extra']['access_token']
     session[:uid] = request.env['omniauth.auth']['uid']
     session[:info] = request.env['omniauth.auth']['info']
+    session[:consumer] = {
+      secret: oauth_data.consumer.secret,
+      key: oauth_data.consumer.key
+    }
+    session[:token] = {
+      token: oauth_data.token,
+      secret: oauth_data.secret
+    }
   end
 
   def check_session
     valid = { session: true }
     invalid = { session: false }
-    if current_user
+    if current_user?
       json valid
     else
       json invalid

78d80250349b26c8bdca446af26276ef72c8be83
Liz Abinante <eabinante@gmail.com>
2016-02-20 09:45:17 -0800

    helper for session stores the important stuff on the user - maybe salt the username somehow?

--- helpers.rb
+++ helpers.rb
@@ -1,39 +1,37 @@
 helpers do
-  def current_user?
-    !session[:uid].nil?
+  def user(user_session)
+    { username: user_session[:username], first_name: user_session[:info]['first_name'], token: user_session[:token][:token] }
   end
 
-  def set_user
-    info = session[:info]
-    @user = { username: info['nickname'], first_name: info['first_name'], logged_in: current_user? }
-  end
-
-  def clear_session
-    session[:info] = nil
-    session[:uid] = nil
-    session[:consumer] = nil
-    session[:token] = nil
+  def clear_session(username)
+    session[username] = nil
     @user = nil
   end
 
   def set_session
+    user_info = request.env['omniauth.auth']['info']
     oauth_data = request.env['omniauth.auth']['extra']['access_token']
-    session[:uid] = request.env['omniauth.auth']['uid']
-    session[:info] = request.env['omniauth.auth']['info']
-    session[:consumer] = {
-      secret: oauth_data.consumer.secret,
-      key: oauth_data.consumer.key
+    # maybe salt the username key here somehow?
+    session[user_info.username] = {
+      uid: request.env['omniauth.auth']['uid'],
+      info: user_info.username,
+      username: user_info.username,
+      consumer: {
+        secret: oauth_data.consumer.secret,
+        key: oauth_data.consumer.key
+      },
+      token: {
+        token: oauth_data.token,
+        secret: oauth_data.secret
+      }
     }
-    session[:token] = {
-      token: oauth_data.token,
-      secret: oauth_data.secret
-    }
+    user(session[user_info.username])
   end
 
-  def check_session
+  def check_session(username, token)
     valid = { session: true }
     invalid = { session: false }
-    if current_user?
+    if session[:username] && session[:username][:token][:token] == token
       json valid
     else
       json invalid
